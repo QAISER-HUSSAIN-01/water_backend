b876a068a60907f0c24438839a0a79041e86cd58
qaiser.hussain <qaiser.hussain@sybrid.com>
2023-08-20 20:35:09 +0500

    changes

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.DB, {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({origin:'*'}));
 app.use(express.json());
 app.use('/api',authRoutes);
 app.use('/api/user',userRoutes);
